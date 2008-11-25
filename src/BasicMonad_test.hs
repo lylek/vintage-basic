3392328594fc1a303d88d2864564bf2cefc43e4b
lyle <lyle@vintage-basic.net>
2008-11-24 19:56:53 -0800

    add-tests-on-BasicMonad
    Ignore-this: 44564aa1b33f92e1e1aed237d6a4aaa9
    
    darcs-hash:20081125035653-962f2-e4bde31faba0c0e79678a23fb0593b5dae5aae72.gz

---
+++ src/BasicMonad_test.hs
@@ -0,0 +1,28 @@
+module BasicMonad_test where
+
+import Data.List(intercalate)
+import Test.HUnit
+import BasicAsserts
+import BasicMonad
+import DurableTraps
+import IOStream
+
+expectOutput expected code = TestCase $ do
+    input <- noInput
+    output <- mkOutput
+    runProgram input output (code >> done)
+    assertOutputEq output expected
+
+test_printString =
+    expectOutput "first\nsecondthird" $ do
+        printString "first\nsecond"
+        printString "third"
+
+test_output_column =
+    expectOutput "hello\n0;5;0" $ do
+        col1 <- getOutputColumn
+        printString "hello"
+        col2 <- getOutputColumn
+        printString "\n"
+        col3 <- getOutputColumn
+        printString (intercalate ";" [show c | c <- [col1, col2, col3]])
