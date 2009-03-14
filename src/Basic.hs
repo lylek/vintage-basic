1cd07ae2c4ed47714d0d781488aa614a879445c1
lyle <lyle@vintage-basic.net>
2009-03-13 18:44:22 -0700

    add-usage-message-unless-exactly-one-filename-provided
    darcs-hash:20090314014422-962f2-bdd5cae0573cb074153e54429d61bf4531896251.gz

--- src/Basic.hs
+++ src/Basic.hs
@@ -1,14 +1,20 @@
-import System.Environment(getArgs)
+import System.Environment(getArgs,getProgName)
+import System.Exit
 import System.IO
-import Language.VintageBasic.Executer(executeFile)
-import Language.VintageBasic.BasicMonad(runProgram)
+import Control.Monad(unless)
 import Control.Monad.CPST.DurableTraps(done)
 import IO.IOStream
+import Language.VintageBasic.Executer(executeFile)
+import Language.VintageBasic.BasicMonad(runProgram)
 
--- | Runs a list of BASIC programs specified on the command line.
+-- | Runs a BASIC program specified on the command line.
 main :: IO ()
 main = do
     args <- getArgs
+    unless (length args == 1) $ do
+        progName <- getProgName
+        hPutStrLn stderr ("Usage: " ++ progName ++ " SOURCE_FILE.bas")
+        exitFailure
     runProgram (IOStream stdin) (IOStream stdout) $ do
         sequence_ [executeFile fileName | fileName <- args]
         done
